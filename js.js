12e9007d2c68390a62b85ed1ba8200190c317abe
Skylark92 <skylark92@live.co.kr>
2022-11-27 23:51:31 +0900

    timer

--- js.js
+++ js.js
@@ -6,24 +6,24 @@ const 초입력 = app.querySelector("#sec");
 
 // 입력값 확인
 시입력.addEventListener("change", () => {
-    if (+시입력.value > 99 || +시입력.value < 0) {
-        시입력.value = "";
-        alert("0 이상 99 이하의 숫자를 입력해주세요.");
-    }
+  if (+시입력.value > 99 || +시입력.value < 0) {
+    시입력.value = "";
+    alert("0 이상 99 이하의 숫자를 입력해주세요.");
+  }
 });
 
 분입력.addEventListener("change", () => {
-    if (+분입력.value >= 60 || +분입력.value < 0) {
-        분입력.value = "";
-        alert("0 이상 60 미만의 숫자를 입력해주세요.");
-    }
+  if (+분입력.value >= 60 || +분입력.value < 0) {
+    분입력.value = "";
+    alert("0 이상 60 미만의 숫자를 입력해주세요.");
+  }
 });
 
 초입력.addEventListener("change", () => {
-    if (+초입력.value >= 60 || +초입력.value < 0) {
-        초입력.value = "";
-        alert("0 이상 60 미만의 숫자를 입력해주세요.");
-    }
+  if (+초입력.value >= 60 || +초입력.value < 0) {
+    초입력.value = "";
+    alert("0 이상 60 미만의 숫자를 입력해주세요.");
+  }
 });
 
 // 버튼 활성화
@@ -33,21 +33,23 @@ const 초기화버튼 = app.querySelector(".reset-btn");
 let 상태 = false;
 
 시작버튼.addEventListener("click", () => {
-    시계();
+  시계();
 });
 
 function 시계() {
-    let 시간 = 시입력.value * 3600 + 분입력.value * 60 + 초입력.value * 1;
+  let 시간 = 시입력.value * 3600 + 분입력.value * 60 + 초입력.value * 1;
 
-    시입력.value = parseInt((시간 - (분입력.value * 60 + 초입력.value * 1)) / 3600);
-    분입력.value = parseInt((시간 - (시입력.value * 3600 + 초입력.value * 1)) / 60);
-    초입력.value = parseInt(시간 - (시입력.value * 3600 + 분입력.value * 60));
+  시입력.value = parseInt((시간 - (분입력.value * 60 + 초입력.value * 1)) / 3600);
+  분입력.value = parseInt((시간 - (시입력.value * 3600 + 초입력.value * 1)) / 60);
+  초입력.value = parseInt(시간 - (시입력.value * 3600 + 분입력.value * 60));
 
-    if (!시간) {
-        return console.log("안돼");
-    } else {
-        console.log("돌고있나", 시간);
-        시간--;
-    }
-    setTimeout(시계, 1000);
+  if (!시간) {
+    return console.log("안돼");
+  } else {
+    console.log("돌고있나", 시간);
+    시간--;
+  }
+  setTimeout(시계, 1000);
 }
+
+// ㅠ__ㅠ
